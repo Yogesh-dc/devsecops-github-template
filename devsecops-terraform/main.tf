504a306977fc4f49062b0262901734d4d9e56c3d
Yogesh-dc <yogeshchoudhary1111200@gmail.com>
2025-07-29 19:01:24 +0530

    terra changes

--- devsecops-terraform/main.tf
+++ devsecops-terraform/main.tf
@@ -3,15 +3,16 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "officekey"
-  public_key = file("${path.module}/officekey.pub")
-}
-
-resource "tls_private_key" "example" {
+resource "tls_private_key" "ssh_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "officekey"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
+
 
 resource "local_file" "private_key" {
   content  = tls_private_key.example.private_key_pem
--- devsecops-terraform/outputs.tf
+++ devsecops-terraform/outputs.tf
@@ -2,3 +2,11 @@
 output "public_ip" {
   value = aws_instance.devsecops_instance.public_ip
 }
+output "public_ip" {
+  value = aws_instance.devsecops.public_ip
+}
+
+output "private_key_pem" {
+  value     = tls_private_key.ssh_key.private_key_pem
+  sensitive = true
+}
