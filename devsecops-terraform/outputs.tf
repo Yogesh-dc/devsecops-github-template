8529ac4f50bf25aedb0792ee15a025be902103ae
Yogesh-dc <yogeshchoudhary1111200@gmail.com>
2025-07-29 19:24:32 +0530

    terra 03

--- devsecops-terraform/outputs.tf
+++ devsecops-terraform/outputs.tf
@@ -3,8 +3,9 @@ output "public_ip" {
   value       = aws_instance.devsecops_instance.public_ip
 }
 
+
 output "private_key_pem" {
   description = "Private key content"
-  value       = tls_private_key.example.private_key_pem
+  value       = tls_private_key.ssh_key.private_key_pem
   sensitive   = true
 }
