8084de8b78118074d3f036564d8f91b9e8111840
yogesh-dc <yogeshchoudhary1111200@gmail.com>
2025-07-30 19:16:42 +0530

    updated sg name and key and declare

--- devsecops-terraform/main.tf
+++ devsecops-terraform/main.tf
@@ -83,7 +83,7 @@ resource "aws_instance" "devsecops_instance" {
   ami                    = "ami-007020fd9c84e18c7" # Ubuntu 20.04 LTS in ap-south-1
   instance_type          = "t2.medium"
   key_name               = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.devsecops_sg.id]
+  vpc_security_group_ids = [aws_security_group.DevSecOps01_sg.id]
 
   tags = {
     Name = "DevSecOps-EC2"
