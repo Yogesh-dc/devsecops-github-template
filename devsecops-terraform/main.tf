abc945864dbc911005c62bc8cfd4d651ddd86550
yogesh-dc <yogeshchoudhary1111200@gmail.com>
2025-07-30 19:03:29 +0530

    updated tf and main

--- devsecops-terraform/main.tf
+++ devsecops-terraform/main.tf
@@ -94,12 +94,6 @@ resource "aws_instance" "devsecops_instance" {
   }
 }
 
-# Outputs
-output "public_ip" {
-  value = aws_instance.devsecops_instance.public_ip
-}
 
-output "private_key_pem" {
-  value     = tls_private_key.ssh_key.private_key_pem
-  sensitive = true
-}
+
+
