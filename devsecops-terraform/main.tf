5292d18f1e19630e64c931ba597367071a8d831d
yogesh-dc <yogeshchoudhary1111200@gmail.com>
2025-07-31 14:43:35 +0530

    updated03

--- devsecops-terraform/main.tf
+++ devsecops-terraform/main.tf
@@ -22,8 +22,8 @@ resource "local_file" "private_key" {
 }
 
 # Security Group with required open ports
-resource "aws_security_group" "DevSecOps01_sg" {
-  name        = "DevSecOps01_sg"
+resource "aws_security_group" "DevSecOps02_sg" {
+  name        = "DevSecOps02_sg"
   description = "Allow SSH, Apache, App, Nexus, SonarQube ports"
   
   ingress {
@@ -83,7 +83,7 @@ resource "aws_instance" "devsecops_instance" {
   ami                    = "ami-007020fd9c84e18c7" # Ubuntu 20.04 LTS in ap-south-1
   instance_type          = "t2.medium"
   key_name               = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.DevSecOps01_sg.id]
+  vpc_security_group_ids = [aws_security_group.DevSecOps02_sg.id]
 
   tags = {
     Name = "DevSecOps-EC2"
